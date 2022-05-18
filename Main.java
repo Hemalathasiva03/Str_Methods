e110efa10326ac11f67880f0d57869c8a83d3b6d
Hemalathasiva03 <103370115+Hemalathasiva03@users.noreply.github.com>
2022-05-18 15:36:04 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+import StringMethod.StrMethods;
+
+public class Main {
+
+public static void main(String[] args)
+{
+
+		String s1="I am working for zoho";
+		String a="zoho";
+		StrMethods obj=new StrMethods(s1);
+		System.out.println("String trimmed : "+obj.trim(s1));
+		System.out.println("String substring : "+obj.substring(0, 4));
+		System.out.println("String replaced : "+obj.Replace('a','y'));
+		System.out.println("String indexof : "+obj.StrIndexOf('a'));
+
+}
+
+}
---
+++ StrMethods.java
@@ -0,0 +1,65 @@
+package StringMethod;
+
+public class StrMethods 
+
+{
+	private String org;
+	public StrMethods(String org)
+	{
+	this.org=org;
+}
+
+	//String Trim
+public  String trim(String s){
+	String ans="";
+	for(int i=0;i<s.length();i++)
+	{
+	if(s.charAt(i)!=' ')
+	{
+	ans+=s.charAt(i);
+	}
+	}
+	return ans;
+
+}
+
+// String Substring return 
+public String substring(int si,int ei)
+{
+	String ans="";
+	for(int i=si;i<ei;i++)
+	{
+	ans+=org.charAt(i);
+	}
+	return ans;
+}
+
+//String Replace
+public String Replace(char ch1, char ch2)
+ {
+    char[] arr = org.toCharArray();
+    int len =org.length();
+    for(int i=0;i<len;i++)
+    {
+    if(arr[i]==ch1){
+    arr[i] = ch2;
+    }
+    }
+    String str2 = new String(arr);
+           return str2;
+}
+
+//String Index
+public int StrIndexOf(char ch) 
+{
+		int len = org.length();
+  		char[] arr =  org.toCharArray();
+  		for(int index =0;index<len;index++)
+  		{
+  			if(ch== arr[index]){
+  				return index;
+  			}
+  		}
+  		return -1;
+  	}
+}
